07e32b9b800441a48968e8957e79563b0dcf0f65
natthaphatr <50091001+natthaphatr@users.noreply.github.com>
2019-04-29 15:27:01 +0700

    Add files via upload

---
+++ search.php
@@ -0,0 +1,9 @@
+<?php
+   require('connect_db.php');
+   $sql_text ="SELECT * FROM commu where keyword LIKE 'a'";
+   $query = mysqli_query($conn,$sql_text);
+   while($objresult=mysqli_fetch_assoc($query))
+   {
+       echo $objresult['answer']."<br>";
+   }
+?>
