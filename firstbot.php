a6c9854b20bcd3d00b92c920809a3a232d0ce6eb
natthaphatr <50091001+natthaphatr@users.noreply.github.com>
2019-04-29 15:59:20 +0700

    fdsk;fksd;lkfeopkfdls;kfpoef

--- firstbot.php
+++ firstbot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             $txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
             if($txtin=='โหลๆๆ')
             {
-                $txtback='5โหล';
+                $txtback='5โหล.....';
             }
             reply_msg($txtback,$replyToken);      
         }
