c2b6fc150238e59eef97aff6f2ee0badb16bfbd3
natthaphatr <50091001+natthaphatr@users.noreply.github.com>
2019-04-30 16:10:30 +0700

    112

--- api/query.php
+++ api/query.php
@@ -1,10 +1,13 @@
 <?php
-$host = 'us-cdbr-gcp-east-01.cleardb.net';
-$user = 'b2ce45e147adf3';
-$pass = '38b5e984b71cdfc';
-$db = 'gcp_51ac2d698fb071e43c4f';
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$user = 'b859a12ba0c2d9';
+$pass = 'e2e15cf7';
+$db = 'heroku_700de64fb6a62d3';
 $conn = new mysqli($host,$user,$pass,$db);
-
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+}
 mysqli_query($conn, "SET NAMES utf8");
     $office = $_POST["office"];
     $keyword = $_POST["keyword"];
